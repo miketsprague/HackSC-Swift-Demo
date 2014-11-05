7991e167e1439e11d0e67b35e9dedd2f4ba20d8b
Mike <mike@guidebook.com>
2014-11-04 17:41:33 -0800

    Basic proj

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    // MARK: private properties
+    
+    // Your data (retrieved from the Interweb)
+    private var data : [YourDataModel] = []
+    
+    @IBOutlet weak private var tableView: UITableView!
+    
+    // MARK: UIViewController lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        for i in (0...3) {
+            let dataModel = YourDataModel()
+            dataModel.name = "Test"
+            self.data += [dataModel]
+        }
     }
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as? UITableViewCell {
+            cell.backgroundColor = UIColor.clearColor()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            let dataModel = self.data[indexPath.row]
+            cell.textLabel.text = "Load json text"
+
+            return cell
+        }
+        // This should never happen:
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
     }
 
 
---
+++ SwiftDemo/YourDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  YourDataModel.swift
+//  SwiftDemo
+//
+//  Created by Mike Sprague on 11/4/14.
+//  Copyright (c) 2014 Guidebook. All rights reserved.
+//
+
+import Foundation
+
+class YourDataModel {
+    var name = "Your Data Model"
+}
